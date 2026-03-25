96dc880ee7c2fafbce1f679a43a7989be43e6fa6
Jake-1-2-3 <jack1720@msu.edu>
2026-03-25 20:48:26 +0000

    Chapter 4, Project 2 Submission

--- os/proc.c
+++ os/proc.c
@@ -3,6 +3,7 @@
 #include "loader.h"
 #include "trap.h"
 #include "vm.h"
+#include "timer.h" // added 
 
 struct proc pool[NPROC];
 __attribute__((aligned(16))) char kstack[NPROC][PAGE_SIZE];
@@ -30,14 +31,28 @@ void proc_init(void)
 		p->state = UNUSED;
 		p->kstack = (uint64)kstack[p - pool];
 		p->trapframe = (struct trapframe *)trapframe[p - pool];
-		/*
-		* LAB1: you may need to initialize your new fields of proc here
-		*/
+		// New lab 2 additions 
+		p->start_time = 0;
+		memset(p->syscall_times, 0, sizeof(p->syscall_times));
 	}
 	idle.kstack = (uint64)boot_stack_top;
 	idle.pid = 0;
 	current_proc = &idle;
 }
+
+		/*
+		* LAB1: you may need to initialize your new fields of proc here
+		if (p->start_time == 0) {
+					p->start_time = get_cycle(); 
+				}
+				p->state = RUNNING;
+				current_proc = p;
+				swtch(&idle.context, &p->context);
+	}
+	idle.kstack = (uint64)boot_stack_top;
+	idle.pid = 0;
+	current_proc = &idle; */
+		
 
 int allocpid()
 {
@@ -86,6 +101,9 @@ void scheduler(void)
 				/*
 				* LAB1: you may need to init proc start time here
 				*/
+				if (p->start_time == 0){
+					p->start_time = get_cycle();
+				}
 				p->state = RUNNING;
 				current_proc = p;
 				swtch(&idle.context, &p->context);
--- os/syscall.c
+++ os/syscall.c
@@ -4,6 +4,8 @@
 #include "syscall_ids.h"
 #include "timer.h"
 #include "trap.h"
+#include "proc.h"
+#include "vm.h" //added 
 
 uint64 sys_write(int fd, uint64 va, uint len)
 {
@@ -31,28 +33,129 @@ uint64 sys_sched_yield()
 	yield();
 	return 0;
 }
-
+ 
 uint64 sys_gettimeofday(TimeVal *val, int _tz) // TODO: implement sys_gettimeofday in pagetable. (VA to PA)
 {
-	// YOUR CODE
-	val->sec = 0;
-	val->usec = 0;
-
-	/* The code in `ch3` will leads to memory bugs*/
-
-	// uint64 cycle = get_cycle();
-	// val->sec = cycle / CPU_FREQ;
-	// val->usec = (cycle % CPU_FREQ) * 1000000 / CPU_FREQ;
+	struct proc *p = curr_proc();
+	uint64 pa = useraddr(p->pagetable, (uint64)val);
+	if (pa == 0)
+		return -1;
+	TimeVal *kval = (TimeVal *)pa;
+	uint64 cycle = get_cycle();
+	kval->sec = cycle / CPU_FREQ;
+	kval->usec = (cycle % CPU_FREQ) * 1000000 / CPU_FREQ;
 	return 0;
 }
 
 // TODO: add support for mmap and munmap syscall.
 // hint: read through docstrings in vm.c. Watching CH4 video may also help.
 // Note the return value and PTE flags (especially U,X,W,R)
+uint64 sys_mmap(uint64 start, uint64 len, int port, int flag, int fd)
+{
+	// Validate: start must be page-aligned
+	if (start % PAGE_SIZE != 0)
+		return -1;
+	// len == 0: return immediately
+	if (len == 0)
+		return 0;
+	// len upper limit 1 GiB
+	if (len > 1024ULL * 1024 * 1024)
+		return -1;
+	// port: other bits must be 0
+	if ((port & ~0x7) != 0)
+		return -1;
+	// port: must have at least one permission
+	if ((port & 0x7) == 0)
+		return -1;
+
+	struct proc *p = curr_proc();
+
+	// Round len up to page boundary
+	uint64 end = start + ((len + PAGE_SIZE - 1) / PAGE_SIZE) * PAGE_SIZE;
+
+	// Check that no page in [start, end) is already mapped
+	for (uint64 va = start; va < end; va += PAGE_SIZE) {
+		if (walkaddr(p->pagetable, va) != 0)
+			return -1;
+	}
+	int perm = PTE_U | PTE_V;
+
+	if (port & 0x1) perm |= PTE_R;
+	if (port & 0x2) perm |= PTE_W;
+	if (port & 0x4) perm |= PTE_X;
+
+	// map pages
+	for (uint64 va = start; va < end; va += PAGE_SIZE) {
+		void *pa = kalloc();
+		if (pa == 0)
+			return -1;
+
+		memset(pa, 0, PAGE_SIZE);
+
+		if (mappages(p->pagetable, va, PAGE_SIZE, (uint64)pa, perm) != 0) {
+			kfree(pa);
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
+uint64 sys_munmap(uint64 start, uint64 len)
+{
+	// Validate: start must be page-aligned
+	if (start % PAGE_SIZE != 0)
+		return -1;
+	if (len == 0)
+		return 0;
+
+	struct proc *p = curr_proc();
+
+	// Round len up to page boundary
+	uint64 end = start + ((len + PAGE_SIZE - 1) / PAGE_SIZE) * PAGE_SIZE;
+
+	// Check that all pages in [start, end) are currently mapped
+	for (uint64 va = start; va < end; va += PAGE_SIZE) {
+		if (walkaddr(p->pagetable, va) == 0)
+			return -1;
+	}
+
+	// Unmap page by page
+	for (uint64 va = start; va < end; va += PAGE_SIZE) {
+		uvmunmap(p->pagetable, va, 1, 1);
+	}
+	return 0;
+}
+
+
 /*
 * LAB1: you may need to define sys_task_info here
 */
+uint64 sys_getpid()
+{
+	return curr_proc()->pid; // Returns PID 
+}
+uint64 sys_task_info(TaskInfo *ti)
+{
+	struct proc *p = curr_proc(); //operate on current running process
+	uint64 pa = useraddr(p->pagetable, (uint64)ti);
+	if (pa == 0)
+		return -1;
+	TaskInfo *kti = (TaskInfo *)pa;
+	// Set status to running 
+	kti->status = 2; 
+	
+	// copys syscall counters 
+	for (int i = 0; i < MAX_SYSCALL_NUM; i++) { 
+		kti->syscall_times[i] = p->syscall_times[i];
+	}
 
+	// computes rutime (ms) 
+	uint64 now = get_cycle();
+	kti->time = (now - p->start_time) * 1000 / CPU_FREQ; // ms conversion 
+
+	return 0;
+}
 extern char trap_page[];
 
 void syscall()
@@ -66,6 +169,11 @@ void syscall()
 	/*
 	* LAB1: you may need to update syscall counter for task info here
 	*/
+	struct proc *p = curr_proc(); // Pointer to process that made syscall 
+	if (id >= 0 && id < MAX_SYSCALL_NUM) { // validating syscall #
+		p->syscall_times[id]++; // Tracks # of calls 
+	}
+
 	switch (id) {
 	case SYS_write:
 		ret = sys_write(args[0], args[1], args[2]);
@@ -82,6 +190,18 @@ void syscall()
 	/*
 	* LAB1: you may need to add SYS_taskinfo case here
 	*/
+	case SYS_getpid: // Calls getpid kernel function 
+		ret = sys_getpid(); 
+		break; 
+	case SYS_task_info: // Calls Kernel function sys_task_info 
+		ret = sys_task_info((TaskInfo *)args[0]);
+		break;
+	case SYS_mmap: // Calls Kernel function sys_mmap
+		ret = sys_mmap(args[0], args[1], args[2], args[3], args[4]);
+		break;
+	case SYS_munmap: // Calls Kernel function sys_munmap
+		ret = sys_munmap(args[0], args[1]);
+		break;
 	default:
 		ret = -1;
 		errorf("unknown syscall %d", id);
